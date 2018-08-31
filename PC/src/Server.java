e604630d47033ced362bc10a010b7905b1797a57
Persea <Persea@THZN_04>
2018-08-31 14:59:11 +0800

    websocket连接时加入程序锁

--- PC/src/Server.java
+++ PC/src/Server.java
@@ -585,6 +585,7 @@ public class Server implements Runnable {
 						protected void initChannel(SocketChannel chweb) throws Exception {
 							// TODO Auto-generated method stub
 
+							synchronized (this) {
 							chweb.pipeline().addLast("httpServerCodec", new HttpServerCodec());
 							chweb.pipeline().addLast("chunkedWriteHandler", new ChunkedWriteHandler());
 							chweb.pipeline().addLast("httpObjectAggregator", new HttpObjectAggregator(8192));
@@ -593,6 +594,7 @@ public class Server implements Runnable {
 							websocketcount++;
 							websocketlist.put(Integer.toString(websocketcount),chweb);
 							NS.websocketlist = websocketlist;
+							}
 						}
 	            		
 	            	});
