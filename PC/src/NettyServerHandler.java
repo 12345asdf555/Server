ea64d213d0cefc27dd8460681ff8f3bb99e424bb
Persea <815137651@qq.com>
2019-07-16 21:44:16 +0800

    修改

--- PC/src/NettyServerHandler.java
+++ PC/src/NettyServerHandler.java
@@ -21,45 +21,45 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 @Sharable 
 public class NettyServerHandler extends ChannelHandlerAdapter{
-	
+
 	public String ip;
-    public String ip1;
-    public String connet;
-    public Thread workThread;
-    public java.sql.Statement stmt =null;
-    public SocketChannel socketchannel = null;
-    public ArrayList<String> dbdata = new ArrayList<String>();
-    public ArrayList<String> listarray1 = new ArrayList<String>();
-    public ArrayList<String> listarray2 = new ArrayList<String>();
-    public ArrayList<String> listarray3 = new ArrayList<String>();
-    public HashMap<String, SocketChannel> socketlist = new HashMap<>();
-    public HashMap<String, SocketChannel> websocketlist = new HashMap<>();
-    public Mysql mysql = new Mysql();
-    public Android android = new Android();
-    public Websocket websocket = new Websocket();
+	public String ip1;
+	public String connet;
+	public Thread workThread;
+	public java.sql.Statement stmt =null;
+	public SocketChannel socketchannel = null;
+	public ArrayList<String> dbdata = new ArrayList<String>();
+	public ArrayList<String> listarray1 = new ArrayList<String>();
+	public ArrayList<String> listarray2 = new ArrayList<String>();
+	public ArrayList<String> listarray3 = new ArrayList<String>();
+	public HashMap<String, SocketChannel> socketlist = new HashMap<>();
+	public HashMap<String, SocketChannel> websocketlist = new HashMap<>();
+	public Mysql mysql = new Mysql();
+	public Android android = new Android();
+	public Websocket websocket = new Websocket();
 	public byte[] b;
-    public int a=0;
-    
+	public int a=0;
+
 	@Override  
 	public void channelRead(ChannelHandlerContext ctx, Object msg) {
-		 
+
 		String str = "";
 		try{
 			str = (String) msg;
 			Workspace ws = new Workspace(str);
-	        workThread = new Thread(ws);  
-	        workThread.start();
-	        
-	        ReferenceCountUtil.release(msg);
+			workThread = new Thread(ws);  
+			workThread.start();
+
+			ReferenceCountUtil.release(msg);
 			ReferenceCountUtil.release(str);
-		 }catch(Exception e){
-			 e.printStackTrace();
-		 }
-		 
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
 		ctx.flush();
-	 }
-	 
-	 public class Workspace implements Runnable{
+	}
+
+	public class Workspace implements Runnable{
 
 		private String str="";
 		public byte[] req;
@@ -68,22 +68,22 @@ public class NettyServerHandler extends ChannelHandlerAdapter{
 
 		public Workspace(String str) {
 			// TODO Auto-generated constructor stub
-			
+
 			this.str=str;
-			
+
 		}
 
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-		
+
 			if(str.substring(0,2).equals("7E") && (str.substring(10,12).equals("22")) && (str.length()==284 || str.length()==124)){
 
 				mysql.Mysqlbase(str);
-		        websocket.Websocketbase(str,listarray2,listarray3,websocketlist);
-		        if(socketchannel!=null){
-		        	synchronized (socketchannel) {
-				        try {
+				websocket.Websocketbase(str,listarray2,listarray3,websocketlist);
+				if(socketchannel!=null){
+					synchronized (socketchannel) {
+						try {
 							socketchannel.writeAndFlush(str).sync();
 						} catch (Exception e) {
 							try {
@@ -95,74 +95,76 @@ public class NettyServerHandler extends ChannelHandlerAdapter{
 							socketchannel = null;
 							e.printStackTrace();
 						}
-		        	}
-		        }
-				
-			}else if(str.substring(0,2).equals("FA")){  //处理实时数据
-				
-				mysql.Mysqlrun(str);
-		        websocket.Websocketrun(str,listarray2,listarray3,websocketlist);
-		        if(socketchannel!=null){
-			        try {
-						socketchannel.writeAndFlush(str).sync();
-					} catch (Exception e) {
-						socketchannel = null;
-						e.printStackTrace();
 					}
-		        }
-		        
-	        }else if(str.substring(0,2).equals("þ")){   //处理android数据
-	        	
-	        	android.Androidrun(str);
-	        	
-	        }else if(str.substring(0,2).equals("JN")){  //江南任务派发 任务号、焊工、焊机、状态
-	        	
-	        	synchronized (socketlist) {
-	        	ArrayList<String> listarraybuf = new ArrayList<String>();
-	        	boolean ifdo = false;
-	        	
-	        	Iterator<Entry<String, SocketChannel>> iter = socketlist.entrySet().iterator();
-                while(iter.hasNext()){
-                	try{
-                    	Entry<String, SocketChannel> entry = (Entry<String, SocketChannel>) iter.next();
-                    	
-                    	socketfail = entry.getKey();
+				}
 
-        				SocketChannel socketcon = entry.getValue();
-                    	socketcon.writeAndFlush(str).sync();
-                    	
-                	}catch (Exception e) {
-                		listarraybuf.add(socketfail);
-                		ifdo = true;
-   					 }
-                }
-	        	
-                if(ifdo){
-                	for(int i=0;i<listarraybuf.size();i++){
-                    	socketlist.remove(listarraybuf.get(i));
-                	}
-                }
-	        	}
-                
-	        } else if(str.length()==38 && str.substring(10,12).equals("01")){    //处理焊层焊道信息
-	        	mysql.db.ceng = Integer.valueOf(str.substring(18, 20),16);
-	        	mysql.db.dao = Integer.valueOf(str.substring(20, 22),16);
-	        	mysql.db.weldstatus = Integer.valueOf(str.substring(16, 18),16);
-	        	if(socketchannel!=null){
-			        try {
+			}else if(str.substring(0,2).equals("FA")){  //处理实时数据
+
+				mysql.Mysqlrun(str);
+				websocket.Websocketrun(str,listarray2,listarray3,websocketlist);
+				if(socketchannel!=null){
+					try {
 						socketchannel.writeAndFlush(str).sync();
 					} catch (Exception e) {
 						socketchannel = null;
 						e.printStackTrace();
 					}
-		        }
-	        } else if(str.substring(0,2).equals("fe") && str.substring(str.length()-2, str.length()).equals("fe")){  //江南任务派发 任务号、焊工、焊机、状态
-	        	//System.out.println("1");
-	        	if(socketchannel!=null){
-		        	//System.out.println(socketchannel);
-		        	synchronized (socketchannel) {
-				        try {
+				}
+
+			}else if(str.substring(0,2).equals("þ")){   //处理android数据
+
+				android.Androidrun(str);
+
+			}else if(str.substring(0,2).equals("JN")){  //江南任务派发 任务号、焊工、焊机、状态
+
+				synchronized (socketlist) {
+					ArrayList<String> listarraybuf = new ArrayList<String>();
+					boolean ifdo = false;
+
+					Iterator<Entry<String, SocketChannel>> iter = socketlist.entrySet().iterator();
+					while(iter.hasNext()){
+						try{
+							Entry<String, SocketChannel> entry = (Entry<String, SocketChannel>) iter.next();
+
+							socketfail = entry.getKey();
+
+							SocketChannel socketcon = entry.getValue();
+							socketcon.writeAndFlush(str).sync();
+
+						}catch (Exception e) {
+							listarraybuf.add(socketfail);
+							ifdo = true;
+						}
+					}
+
+					if(ifdo){
+						for(int i=0;i<listarraybuf.size();i++){
+							socketlist.remove(listarraybuf.get(i));
+						}
+					}
+				}
+
+			} else if(str.length()==38 && str.substring(10,12).equals("01")){    //处理焊层焊道信息
+				mysql.db.ceng = Integer.valueOf(str.substring(18, 20),16);
+				mysql.db.dao = Integer.valueOf(str.substring(20, 22),16);
+				mysql.db.weldstatus = Integer.valueOf(str.substring(16, 18),16);
+				if(socketchannel!=null){
+					try {
+						socketchannel.writeAndFlush(str).sync();
+					} catch (Exception e) {
+						socketchannel = null;
+						e.printStackTrace();
+					}
+				}
+			} else if(str.substring(0,2).equals("fe") && str.substring(str.length()-2, str.length()).equals("fe")){  //江南任务派发 任务号、焊工、焊机、状态
+				//System.out.println("1");
+				System.out.println("接收数据:"+str);
+				if(socketchannel!=null){
+					//System.out.println(socketchannel);
+					synchronized (socketchannel) {
+						try {
 							socketchannel.writeAndFlush(str).sync();
+							System.out.println("发送成功:"+str);
 						} catch (Exception e) {
 							try {
 								socketchannel.close().sync();
@@ -173,49 +175,49 @@ public class NettyServerHandler extends ChannelHandlerAdapter{
 							socketchannel = null;
 							e.printStackTrace();
 						}
-		        	}
-		        }
-                
-	        } else{    //处理焊机下发和上传
-	        	
-	        	//System.out.println(str);
-	        	
-	        	synchronized (websocketlist) {
-	        	ArrayList<String> listarraybuf = new ArrayList<String>();
-	        	boolean ifdo = false;
-	        	
-	        	Iterator<Entry<String, SocketChannel>> webiter = websocketlist.entrySet().iterator();
-                while(webiter.hasNext()){
-                	try{
-                    	Entry<String, SocketChannel> entry = (Entry<String, SocketChannel>) webiter.next();
-                    	websocketfail = entry.getKey();
-                    	SocketChannel websocketcon = entry.getValue();
-                    	websocketcon.writeAndFlush(new TextWebSocketFrame(str)).sync();
-                	}catch (Exception e) {
-                		
-                		listarraybuf.add(websocketfail);
-                		ifdo = true;
-   					 }
-                }
-                
-                if(ifdo){
-                	for(int i=0;i<listarraybuf.size();i++){
-                		websocketlist.remove(listarraybuf.get(i));
-                	}
-                }
-	        	}
-	        }
+					}
+				}
+
+			} else{    //处理焊机下发和上传
+
+				//System.out.println(str);
+
+				synchronized (websocketlist) {
+					ArrayList<String> listarraybuf = new ArrayList<String>();
+					boolean ifdo = false;
+
+					Iterator<Entry<String, SocketChannel>> webiter = websocketlist.entrySet().iterator();
+					while(webiter.hasNext()){
+						try{
+							Entry<String, SocketChannel> entry = (Entry<String, SocketChannel>) webiter.next();
+							websocketfail = entry.getKey();
+							SocketChannel websocketcon = entry.getValue();
+							websocketcon.writeAndFlush(new TextWebSocketFrame(str)).sync();
+						}catch (Exception e) {
+
+							listarraybuf.add(websocketfail);
+							ifdo = true;
+						}
+					}
+
+					if(ifdo){
+						for(int i=0;i<listarraybuf.size();i++){
+							websocketlist.remove(listarraybuf.get(i));
+						}
+					}
+				}
+			}
 		}
-	 }
-	 
-	 
-	 @Override  
+	}
+
+
+	@Override  
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception { 
-	     ctx.flush();  
-	 } 
-     @Override  
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {  
-         ctx.close();  
-     } 
-	 
+		ctx.flush();  
+	} 
+	@Override  
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {  
+		ctx.close();  
+	} 
+
 }
